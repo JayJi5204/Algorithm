2856d8e2f24a62efbf476e46cdf988f81d7fa6ee
SungHyunJi <126458483+JayJi5204@users.noreply.github.com>
2026-04-25 18:07:44 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        
+        int[] answer = new int[commands.length];
+        
+        for(int a=0;a<commands.length;a++){
+            
+            ArrayList<Integer> list=new ArrayList<>();
+            
+            int i=commands[a][0];
+            int j=commands[a][1];
+            int k=commands[a][2];
+            
+            for(int b=i-1;b<j;b++){
+                list.add(array[b]);      
+            }
+           
+            Collections.sort(list);
+            
+            answer[a]=list.get(k-1);
+           
+        }       
+        
+        return answer;
+    }
+}
