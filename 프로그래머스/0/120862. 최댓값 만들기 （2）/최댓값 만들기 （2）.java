aff24ae3e4815f7734c98bbdfea88b4da764001c
SungHyunJi <126458483+JayJi5204@users.noreply.github.com>
2026-05-08 15:47:57 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.37 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = numbers[0]*numbers[1];
+
+        for(int i=0;i<numbers.length;i++){
+            for(int j=0;j<numbers.length;j++){
+                if(i==j){
+                    continue;
+                }
+                answer=Math.max(numbers[i]*numbers[j],answer);
+            }
+        }
+
+        return answer;
+    }
+}
