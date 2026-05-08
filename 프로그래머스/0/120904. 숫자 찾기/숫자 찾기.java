b3a9be8886519104eb0f5de5a6715f2c9adb28bb
SungHyunJi <126458483+JayJi5204@users.noreply.github.com>
2026-05-08 16:22:26 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int num, int k) {
+        int answer = -1;
+        
+        String str=String.valueOf(num);
+        char[] arr=str.toCharArray();
+        
+        for(int i=0;i<arr.length;i++){
+            int value=arr[i]-'0';
+            if(value==k){
+                answer=i+1;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
