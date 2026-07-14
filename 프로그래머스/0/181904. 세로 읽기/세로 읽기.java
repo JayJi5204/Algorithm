4ab7f3f4a3a83589f74d3b93ae58deca72931933
SungHyunJi <126458483+JayJi5204@users.noreply.github.com>
2026-07-14 17:52:55 +0900

    [level 0] Title: 세로 읽기, Time: 6.43 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int m, int c) {
+        String answer = "";
+        
+        char[] charArr=my_string.toCharArray();
+        
+        for(int i=c-1;i<charArr.length;i+=m){
+            answer+=charArr[i];
+        }
+        
+        return answer;
+    }
+}
